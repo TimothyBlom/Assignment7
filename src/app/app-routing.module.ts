588ce8d204b479166af1cb7984ce099b72784c27
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-03-29 18:18:13 +0200

    Tweak views and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,31 @@
-import { Component, NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
 const routes: Routes = [
-    {
-        path: "",
-        component: LoginPage
-    },
-    {
-        path: "catalogue",
-        component: CataloguePage
-    },
-    {
-        path: "trainer",
-        component: TrainerPage
-    }
-]
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login',
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'catalogue',
+    component: CataloguePage,
+  },
+  {
+    path: 'trainer',
+    component: TrainerPage,
+  },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],    //import a module
-    exports: [RouterModule]     //expose module and features
+  imports: [RouterModule.forRoot(routes)], //import a module
+  exports: [RouterModule], //expose module and features
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
