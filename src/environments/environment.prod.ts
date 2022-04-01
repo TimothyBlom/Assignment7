2705194a2cf3a524aaad90e61db65dd3573d780e
Timothy Blom <kracok@gmail.com>
2022-04-01 12:39:44 +0200

    updated url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiKey: '2cdf30b3-079f-410a-bfc1-376b65de5b4e',
-  apiTrainers: 'experis-noroff-api.herokuapp.com/trainers',
+  apiTrainers: 'https://experis-noroff-api.herokuapp.com/trainers',
   apiPokemon: 'https://pokeapi.co/api/v2/pokemon?limit=20',
 };
